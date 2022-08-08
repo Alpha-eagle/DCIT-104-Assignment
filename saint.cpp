f91dde80ca5e9c7f64e55acfddf9768a83b3b34e
Your Name <radoe001@st.ug.edu.gh>
2022-08-08 16:27:57 +0000

    first commit

---
+++ saint.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    float sum = 0, count = 0, average;
+    int number;
+    printf("Enter Any Number:");
+    scanf("%d" ,&number);
+    for (int i=1; i<number ; i++){
+        for (int j= 2; j<=i ; j++)
+        {
+            if(j==i){
+                sum+=i;
+                count++,
+            }else if (i%j==0)
+            {
+                break;
+            }
+            
+        }
+        
+    }
+    average=sum/count;
+    printf("Average=%.2f",average);
+    return 0;
+}
